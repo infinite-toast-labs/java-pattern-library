ce5882eec8bcfec8545be08a55209e8e1ecf6d18
infinite-toast-labs <248584818+infinite-toast-labs@users.noreply.github.com>
2026-02-18 05:08:51 -0500

    arrays init

---
+++ src/main/java/com/example/patterns/phase1/MyArraysImpl.java
@@ -0,0 +1,10 @@
+package com.example.patterns.phase1;
+
+import jdk.jshell.spi.ExecutionControl;
+
+public class MyArraysImpl {
+
+    public static void main(String[] args) throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("Todo");
+    }
+}

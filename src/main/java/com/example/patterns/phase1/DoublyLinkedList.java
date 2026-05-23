fc042d407e3b0e10387bc5973f736295e7341d76
infinite-toast-labs <248584818+infinite-toast-labs@users.noreply.github.com>
2026-05-23 12:32:18 -0500

    progress

--- src/main/java/com/example/patterns/phase1/DoublyLinkedList.java
+++ src/main/java/com/example/patterns/phase1/DoublyLinkedList.java
@@ -1,4 +1,40 @@
 package com.example.patterns.phase1;
 
-public class DoublyLinkedList {
+public class DoublyLinkedList<E> {
+
+    // What is a doubly linked list. It's a linked list with both next and prev references.
+    //
+    //
+    private static class Node<E> {
+        private E element;
+        private Node<E> next;
+        private Node<E> previous;
+
+        public Node(E e, Node<E> n) {
+            element = e;
+            next = n;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> n) {
+            next = n;
+        }
+
+        public Node<E> getPrevious() {
+            return previous;
+        }
+
+        public void setPrevious(Node<E> p) {
+            previous = p;
+        }
+        
+    }
+
 }
---
+++ src/test/java/com/example/patterns/phase1/DoublyLinkedListTest.java
@@ -0,0 +1,13 @@
+package com.example.patterns.phase1;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class DoublyLinkedListTest {
+    
+    @Test
+    public void doublyLinkedListTest() {
+        Assertions.fail();
+    }
+
+}
